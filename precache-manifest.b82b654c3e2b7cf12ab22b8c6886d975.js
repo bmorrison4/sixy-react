7f986d00ac0c574043aa7cf5a90da8befe2870dd
Bryan Morrison <skeeter_mcbee@live.com>
2019-04-21 23:50:22 -0700

    Updates

---
+++ precache-manifest.b82b654c3e2b7cf12ab22b8c6886d975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "590284167ad7811aa098",
+    "url": "/sixy-webpage/static/css/main.2c0ef46d.chunk.css"
+  },
+  {
+    "revision": "590284167ad7811aa098",
+    "url": "/sixy-webpage/static/js/main.eb4201e4.chunk.js"
+  },
+  {
+    "revision": "b972cc2964bfdc2660bd",
+    "url": "/sixy-webpage/static/js/runtime~main.ff1e059b.js"
+  },
+  {
+    "revision": "36f1afa8253fbfae9f94",
+    "url": "/sixy-webpage/static/js/2.070b33e6.chunk.js"
+  },
+  {
+    "revision": "e00816c0c0330adae85529077dccafd9",
+    "url": "/sixy-webpage/index.html"
+  }
+];
