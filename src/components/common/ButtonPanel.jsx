66aafacae64cec028e87d5a5be08de12b6c7abeb
Bryan Morrison <skeeter_mcbee@live.com>
2019-04-23 15:41:43 -0700

    re-enable reboot button

--- src/components/common/ButtonPanel.jsx
+++ src/components/common/ButtonPanel.jsx
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 import Slider from "./Slider";
 import Toggle from "./Toggle";
 import '../../styles/common.css'
+import { sendMessage } from './socket'
 
 export default class ButtonPanel extends Component {
+
+  reboot = () => {
+    sendMessage("reboot");
+  }
+
   render() {
     return (
       <div className="button-panel" width="180">
@@ -11,7 +17,7 @@ export default class ButtonPanel extends Component {
         <Slider min="-1.0" max="1.0" name="Speed" value="1.0" step="0.1" />
         <Toggle name="Table" />
         <Toggle name="Mic" />
-        <button className="btn reboot-btn">reboot</button>
+        <button className="btn reboot-btn" onClick={this.reboot}>reboot</button>
       </div>
     );
   }
