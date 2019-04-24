efbc2d0174cab9b6b0b271effdd9ab94b0d3d09e
Bryan Morrison <skeeter_mcbee@live.com>
2019-04-23 20:12:23 -0700

    handwrite chatbox

--- src/components/common/ChatBox.jsx
+++ src/components/common/ChatBox.jsx
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
-import settings from "../../settings.json";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+
+/*<iframe src={ this.src } title="chat" />;*/
 
 class ChatBox extends Component {
-  src = settings.chat.domain + settings.chat.channel;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownChecked: false
+    };
+  }
+
+  updateDropdown = () => {
+    console.log("Changing state", this.state.dropdownChecked);
+    this.setState({ dropdownChecked: !this.state.dropdownChecked });
+  };
+
   render() {
-    return <iframe src={ this.src } title="chat" />;
+    const { dropdownChecked } = this.state;
+    return (
+      <div className="chatBox">
+        <div className="latest-activity" checked={dropdownChecked}>
+          <div className="title">
+            <span className="chat-header">Latest Activity</span>
+          </div>
+          <ul className="latest-activity-messages" />
+        </div>
+        <div className="latest-activity-dropper" onClick={this.updateDropdown}>
+          <i className="dropdown-arrow">
+            {dropdownChecked ? <FaAngleDown /> : <FaAngleUp />}
+          </i>
+        </div>
+        <ul id="message-list" className="chatBox-message-list">
+          <li>
+            <p className="message-header">skeeter_mcbee [sixy]</p>
+            <span className="message-content">This is a message</span>
+          </li>
+          <li>
+            <p className="message-header">jill [sixy]</p>
+            <span className="message-content">indeed it is : ]</span>
+          </li>
+          <li>
+            <p className="message-header">Gedyy [sixy]</p>
+            <span className="message-content">
+              i dinny ken wit yer talkin about
+            </span>
+          </li>
+          <li>
+            <p className="message-header">Admanta [sixy]</p>
+            <span className="message-content">I want beer</span>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
