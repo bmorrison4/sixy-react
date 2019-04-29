0331980807cb008dfc21e8182d72c8c2780a8538
Bryan Morrison <skeeter_mcbee@live.com>
2019-04-28 17:35:23 -0700

    inputs are now determined by json array

--- src/components/common/ButtonPanel.jsx
+++ src/components/common/ButtonPanel.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import Slider from "./Slider";
 import Toggle from "./Toggle";
-import '../../styles/common.css'
-import settings from '../../settings';
-import { sendMessage } from './socket'
+import "../../styles/common.css";
+import settings from "../../settings";
+import { sendMessage } from "./socket";
 
 /**
  * Container for Slider and Toggle components
  */
 export default class ButtonPanel extends Component {
-
   /**
    * calls the sendMessage function with the "reboot" message to signal the
    * robot to reboot.
    */
   reboot = () => {
     sendMessage("reboot");
-  }
+  };
 
   /**
    * Renders 2 sliders, 2 toggles, and the reboot button.
@@ -26,25 +25,37 @@ export default class ButtonPanel extends Component {
    * Toggle 2: Mic
    */
   render() {
+    const inputs = settings.inputs;
+
     return (
       <div className="button-panel" width="180">
-        <Slider 
-          min={ settings.sliders.Volume.min } 
-          max={ settings.sliders.Volume.max } 
-          name={ settings.sliders.Volume.name }
-          value={ settings.sliders.Volume.value }
-          step={ settings.sliders.Volume.step }
-        />
-        <Slider 
-          min={ settings.sliders.Speed.min }
-          max={ settings.sliders.Speed.max }
-          name={ settings.sliders.Speed.name }
-          value={ settings.sliders.Speed.value }
-          step={ settings.sliders.Speed.step }
-        />
-        <Toggle name={ settings.toggles.Table.name }/>
-        <Toggle name={ settings.toggles.Mic.name } />
-        <button className="btn reboot-btn" onClick={this.reboot}>reboot</button>
+        {inputs.map(input => {
+          switch (input.type) {
+            case "slider":
+              return (
+                <>
+                  <Slider
+                    min={input.min}
+                    max={input.max}
+                    step={input.step}
+                    value={input.value}
+                    name={input.name}
+                  />
+                </>
+              );
+            case "toggle":
+              return (
+                <>
+                  <Toggle name={input.name} />
+                </>
+              );
+            default:
+              return `<p>Unknown Type:${input.type}</p>`;
+          }
+        })}
+        <button className="btn reboot-btn" onClick={this.reboot}>
+          reboot
+        </button>
       </div>
     );
   }
